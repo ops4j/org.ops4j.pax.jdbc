a193a2df36761f041f8600df7ad6a416d6d54923
Harald Wellmann <harald.wellmann@gmx.de>
2012-06-16 19:49:12 +0200

    [PAXJDBC-3] Embedded Derby driver adapter

---
+++ pax-jdbc-derby/src/main/java/org/ops4j/pax/jdbc/derby/impl/DerbyDataSourceFactory.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2012 Harald Wellmann.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ops4j.pax.jdbc.derby.impl;
+
+import java.sql.Driver;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import javax.sql.ConnectionPoolDataSource;
+import javax.sql.DataSource;
+import javax.sql.XADataSource;
+
+import org.apache.derby.jdbc.EmbeddedConnectionPoolDataSource40;
+import org.apache.derby.jdbc.EmbeddedDataSource40;
+import org.apache.derby.jdbc.EmbeddedDriver;
+import org.apache.derby.jdbc.EmbeddedXADataSource;
+import org.apache.derby.jdbc.ReferenceableDataSource;
+import org.osgi.service.jdbc.DataSourceFactory;
+
+public class DerbyDataSourceFactory implements DataSourceFactory
+{
+
+    @Override
+    public DataSource createDataSource( Properties props ) throws SQLException
+    {
+        EmbeddedDataSource40 ds = new EmbeddedDataSource40();
+        setProperties( ds, props );
+        return ds;
+    }
+
+    private void setProperties( ReferenceableDataSource ds, Properties properties ) throws SQLException
+    {
+        Properties props = (Properties) properties.clone();
+        String databaseName = (String) props.remove( DataSourceFactory.JDBC_DATABASE_NAME );
+        if( databaseName == null )
+        {
+            throw new SQLException( "missing required property "
+                    + DataSourceFactory.JDBC_DATABASE_NAME );
+        }
+        ds.setDatabaseName( databaseName );
+
+        String password = (String) props.remove( DataSourceFactory.JDBC_PASSWORD );
+        ds.setPassword( password );
+
+        String user = (String) props.remove( DataSourceFactory.JDBC_USER );
+        ds.setUser( user );
+
+        if( !props.isEmpty() )
+        {
+            throw new SQLException( "cannot set properties " + props.keySet() );
+        }
+    }
+
+    @Override
+    public ConnectionPoolDataSource createConnectionPoolDataSource( Properties props )
+        throws SQLException
+    {
+        EmbeddedConnectionPoolDataSource40 ds = new EmbeddedConnectionPoolDataSource40();
+        setProperties( ds, props );
+        return ds;
+    }
+
+    @Override
+    public XADataSource createXADataSource( Properties props ) throws SQLException
+    {
+        EmbeddedXADataSource ds = new EmbeddedXADataSource();
+        setProperties( ds, props );
+        return ds;
+    }
+
+    @Override
+    public Driver createDriver( Properties props ) throws SQLException
+    {
+        EmbeddedDriver driver = new EmbeddedDriver();
+        return driver;
+    }
+
+}
