4f535cd14c876cd4ab3d42c16527794b50fc8035
Harald Wellmann <harald.wellmann@gmx.de>
2012-09-02 14:43:07 +0200

    [PAXJDBC-9] Use OPS4J Base 1.4.0
    Factored out META-INF/services manipulation.

--- pax-jdbc/src/main/java/org/ops4j/pax/jdbc/impl/JdbcDriverObserver.java
+++ pax-jdbc/src/main/java/org/ops4j/pax/jdbc/impl/JdbcDriverObserver.java
@@ -17,19 +17,15 @@
  */
 package org.ops4j.pax.jdbc.impl;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URL;
 import java.sql.Driver;
-import java.util.ArrayList;
 import java.util.Dictionary;
 import java.util.Hashtable;
 import java.util.List;
 
-import org.ops4j.lang.Ops4jException;
+import org.ops4j.pax.swissbox.core.BundleClassLoader;
 import org.ops4j.pax.swissbox.extender.BundleObserver;
+import org.ops4j.spi.SafeServiceLoader;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.service.jdbc.DataSourceFactory;
@@ -43,37 +39,17 @@ public class JdbcDriverObserver implements BundleObserver<URL>
     @Override
     public void addingEntries( Bundle bundle, List<URL> entries )
     {
-        URL url = entries.get( 0 );
         log.info( "found JDBC driver service in bundle [{} {}]", bundle.getSymbolicName(),
             bundle.getVersion() );
-
-        List<String> names = parse( Driver.class, url );
-        for( String impl : names )
-        {
-            log.info( "JDBC driver class: {}", impl );
-            try
-            {
-                Class<?> driverClass = bundle.loadClass( impl );
-                Driver driver = (Driver) driverClass.newInstance();
-                BundleContext bc = bundle.getBundleContext();
-                DriverDataSourceFactory dsf = new DriverDataSourceFactory( driver );
-                Dictionary<String, String> props = new Hashtable<String, String>();
-                props.put( DataSourceFactory.OSGI_JDBC_DRIVER_CLASS, impl );
-                bc.registerService( DataSourceFactory.class.getName(), dsf, props );
-
-            }
-            catch ( ClassNotFoundException exc )
-            {
-                throw new Ops4jException( exc );
-            }
-            catch ( InstantiationException exc )
-            {
-                throw new Ops4jException( exc );
-            }
-            catch ( IllegalAccessException exc )
-            {
-                throw new Ops4jException( exc );
-            }
+        BundleContext bc = bundle.getBundleContext();
+        
+        SafeServiceLoader serviceLoader = new SafeServiceLoader( new BundleClassLoader( bundle ) );
+        List<Driver> drivers = serviceLoader.load( Driver.class.getName() );
+        for (Driver driver : drivers) {
+            DriverDataSourceFactory dsf = new DriverDataSourceFactory( driver );
+            Dictionary<String, String> props = new Hashtable<String, String>();
+            props.put( DataSourceFactory.OSGI_JDBC_DRIVER_CLASS, driver.getClass().getName() );
+            bc.registerService( DataSourceFactory.class.getName(), dsf, props );            
         }
     }
 
@@ -83,60 +59,5 @@ public class JdbcDriverObserver implements BundleObserver<URL>
         log.info( "removing drivers registered on behalf of bundle {} {}",
             bundle.getSymbolicName(), bundle.getVersion() );
         // services get unregistered automatically when the extended bundle stops
-    }
-
-    private void parseLine( List<String> names, String line )
-    {
-        int commentPos = line.indexOf( '#' );
-        if( commentPos >= 0 )
-        {
-            line = line.substring( 0, commentPos );
-        }
-        line = line.trim();
-        if( !line.isEmpty() && !names.contains( line ) )
-        {
-            names.add( line );
-        }
-    }
-
-    private List<String> parse( Class<?> klass, URL url )
-    {
-        InputStream is = null;
-        BufferedReader reader = null;
-        List<String> names = new ArrayList<String>();
-        try
-        {
-            is = url.openStream();
-            reader = new BufferedReader( new InputStreamReader( is, "UTF-8" ) );
-            String line = null;
-            while( ( line = reader.readLine() ) != null )
-            {
-                parseLine( names, line );
-            }
-        }
-        catch ( IOException exc )
-        {
-            throw new Ops4jException( exc );
-        }
-        finally
-        {
-            closeSilently( reader, url );
-        }
-        return names;
-    }
-
-    private void closeSilently( BufferedReader reader, URL url )
-    {
-        try
-        {
-            if( reader != null )
-            {
-                reader.close();
-            }
-        }
-        catch ( IOException exc )
-        {
-            log.error( "cannot close " + url, exc );
-        }
     }
 }
