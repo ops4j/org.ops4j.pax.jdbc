ce0b197b5b419c529703d6d1bae714c03dbf6555
Harald Wellmann <harald.wellmann@gmx.de>
2012-12-16 22:34:30 +0100

    extended developer list and fixed copyright notice

--- pax-jdbc-sqlite/src/main/java/org/ops4j/pax/jdbc/sqlite/impl/Activator.java
+++ pax-jdbc-sqlite/src/main/java/org/ops4j/pax/jdbc/sqlite/impl/Activator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 Harald Wellmann.
+ * Copyright 2012 David Schmitz.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
