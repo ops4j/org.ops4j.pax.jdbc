ff22a143e180e564bbcb31ceaae7b0eb704c8eb6
Christian Schneider <chris@die-schneider.net>
2014-07-18 14:58:56 +0200

    PAXJDBC-30 Support closing of DataSources. Fix bug in pooling props

---
+++ pax-jdbc-pool/src/main/java/org/ops4j/pax/jdbc/pool/impl/CloseablePoolingDataSource.java
@@ -0,0 +1,21 @@
+package org.ops4j.pax.jdbc.pool.impl;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.sql.Connection;
+
+import org.apache.commons.dbcp2.PoolingDataSource;
+import org.apache.commons.pool2.ObjectPool;
+
+public class CloseablePoolingDataSource<C extends Connection> extends PoolingDataSource<C> implements Closeable {
+
+    public CloseablePoolingDataSource(ObjectPool<C> pool) {
+        super(pool);
+    }
+
+    @Override
+    public void close() throws IOException {
+        getPool().close();
+    }
+
+}
