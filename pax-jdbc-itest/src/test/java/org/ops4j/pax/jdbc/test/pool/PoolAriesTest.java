9457c2749c0991c26829001780dececf1b84cc61
Christian Schneider <chris@die-schneider.net>
2017-01-25 17:25:43 +0100

    Add itest for Aries jdbc pool

---
+++ pax-jdbc-itest/src/test/java/org/ops4j/pax/jdbc/test/pool/PoolAriesTest.java
@@ -0,0 +1,37 @@
+package org.ops4j.pax.jdbc.test.pool;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.util.Filter;
+import org.ops4j.pax.jdbc.pool.common.PooledDataSourceFactory;
+import org.ops4j.pax.jdbc.test.AbstractJdbcTest;
+
+public class PoolAriesTest extends AbstractJdbcTest {
+
+    @Inject @Filter("(pool=aries)(xa=false)")
+    PooledDataSourceFactory pool;
+    
+    @Configuration
+    public Option[] config() {
+        return new Option[] { //
+                regressionDefaults(), //
+                mvnBundle("org.apache.geronimo.specs" , "geronimo-validation_1.0_spec"),
+                mvnBundle("org.apache.geronimo.components", "geronimo-connector"),
+                mvnBundle("org.apache.geronimo.specs", "geronimo-j2ee-connector_1.6_spec"),
+                mvnBundle("org.apache.aries.transaction", "org.apache.aries.transaction.jdbc"),
+                mvnBundle("org.apache.geronimo.specs", "geronimo-jta_1.1_spec"),
+                mvnBundle("org.apache.aries", "org.apache.aries.util"),
+                mvnBundle("org.apache.aries.transaction", "org.apache.aries.transaction.manager"),
+                mvnBundle("org.ops4j.pax.jdbc", "pax-jdbc-pool-common"),
+                mvnBundle("org.ops4j.pax.jdbc", "pax-jdbc-pool-aries"),
+                };
+    }
+
+    @Test
+    public void testPooledDataSourceFactoryServicesPresent() {
+    }
+
+}
