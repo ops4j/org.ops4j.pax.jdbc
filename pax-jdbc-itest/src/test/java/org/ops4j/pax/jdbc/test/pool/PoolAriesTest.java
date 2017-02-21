bedb6ab0952aa0ba5c9e2b8545ac9123a4f341d4
charlie <cmordant1@gmail.com>
2017-02-21 22:15:45 +0100

    PAXJDBC-122 - Classnotfound on tranql when using aries-jdbc
    Signed-off-by: charlie <cmordant1@gmail.com>

--- pax-jdbc-itest/src/test/java/org/ops4j/pax/jdbc/test/pool/PoolAriesTest.java
+++ pax-jdbc-itest/src/test/java/org/ops4j/pax/jdbc/test/pool/PoolAriesTest.java
@@ -21,7 +21,9 @@ public class PoolAriesTest extends AbstractJdbcTest {
                 poolDefaults(), //
                 mvnBundle("org.apache.geronimo.components", "geronimo-connector"),
                 mvnBundle("org.apache.geronimo.specs", "geronimo-j2ee-connector_1.6_spec"),
+                mvnBundle("org.tranql","tranql-connector"),
                 mvnBundle("org.ops4j.pax.jdbc", "pax-jdbc-pool-aries"),
+                mvnBundle("commons-logging", "commons-logging")
                 };
     }
 
