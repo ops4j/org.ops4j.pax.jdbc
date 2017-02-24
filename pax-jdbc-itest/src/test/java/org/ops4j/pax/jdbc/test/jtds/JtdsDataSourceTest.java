703049674cde658252a762b97916298b77a33330
Christian Schneider <chris@die-schneider.net>
2017-02-24 13:26:31 +0100

    [PAXJDBC-121] Fixed itests for jtds and adapted to config system

--- pax-jdbc-itest/src/test/java/org/ops4j/pax/jdbc/test/jtds/JtdsDataSourceTest.java
+++ pax-jdbc-itest/src/test/java/org/ops4j/pax/jdbc/test/jtds/JtdsDataSourceTest.java
@@ -8,39 +8,38 @@ import java.util.Properties;
 
 import javax.inject.Inject;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.util.Filter;
 import org.ops4j.pax.jdbc.test.AbstractJdbcTest;
+import org.ops4j.pax.jdbc.test.ServerConfiguration;
 import org.osgi.service.jdbc.DataSourceFactory;
 
 public class JtdsDataSourceTest extends AbstractJdbcTest {
-	
-	@Inject
+    @Rule
+    public ServerConfiguration config = new ServerConfiguration("jtds");
+
+    @Inject
     @Filter("(osgi.jdbc.driver.class=net.sourceforge.jtds.jdbc.Driver)")
     private DataSourceFactory dsf;
-	
-	private final String SERVER_NAME="localhost";
-	private final String DB_NAME = "test";
-	private final int PORT=1433;
-	private final String USER = "pax";
-	private final String PASSWORD = "pax";
-	
-	@Configuration
+
+    @Configuration
     public Option[] config() {
         return options(regressionDefaults(), //
             mvnBundle("org.ops4j.pax.jdbc", "pax-jdbc"), //
             wrappedBundle("mvn:net.sourceforge.jtds/jtds/") //
         );
     }
-	
-	@Test
+
+    @Test
     public void createDataSourceAndConnection() throws SQLException {	
         Properties props = new Properties();
-        props.setProperty(DataSourceFactory.JDBC_URL, "jdbc:jtds:sqlserver://" + SERVER_NAME + ":" + PORT + "/" + DB_NAME);
-        props.setProperty(DataSourceFactory.JDBC_USER, USER);
-        props.setProperty(DataSourceFactory.JDBC_PASSWORD, PASSWORD);
+        String url = String.format("jdbc:jtds:sqlserver://%s:%s/%s", config.getServerName(), config.getPortNumberSt(), config.getDatabaseName());
+        props.setProperty(DataSourceFactory.JDBC_URL, url);
+        props.setProperty(DataSourceFactory.JDBC_USER, config.getUser());
+        props.setProperty(DataSourceFactory.JDBC_PASSWORD, config.getPassword());
         dsf.createDataSource(props).getConnection().close();
     }
 
--- pax-jdbc-itest/src/test/java/org/ops4j/pax/jdbc/test/jtds/JtdsNativeDataSourceTest.java
+++ pax-jdbc-itest/src/test/java/org/ops4j/pax/jdbc/test/jtds/JtdsNativeDataSourceTest.java
@@ -8,46 +8,41 @@ import java.util.Properties;
 
 import javax.inject.Inject;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.util.Filter;
 import org.ops4j.pax.jdbc.test.AbstractJdbcTest;
+import org.ops4j.pax.jdbc.test.ServerConfiguration;
 import org.osgi.service.jdbc.DataSourceFactory;
 
 public class JtdsNativeDataSourceTest extends AbstractJdbcTest {
-	
-	@Inject
+    @Rule
+    public ServerConfiguration config = new ServerConfiguration("jtds");
+
+    @Inject
     @Filter("(osgi.jdbc.driver.name=jtds)")
     private DataSourceFactory dsf;
-	
-	private final String SERVER_NAME="localhost";
-	private final String DB_NAME = "test";
-	private final int PORT=1433;
-	private final String USER = "pax";
-	private final String PASSWORD = "pax";
-	
-	@Configuration
+
+    @Configuration
     public Option[] config() {
         return options(regressionDefaults(), //
             mvnBundle("org.ops4j.pax.jdbc", "pax-jdbc-jtds"), //
             wrappedBundle("mvn:net.sourceforge.jtds/jtds/") //
         );
     }
-	
-	@Test
-    public void createDataSourceAndConnection() throws SQLException {	
+
+    @Test
+    public void createDataSourceAndConnection() throws SQLException {
         Properties props = new Properties();
-        //props.setProperty(DataSourceFactory.JDBC_URL, "jdbc:jtds:sqlserver://" + SERVER_NAME + ":" + PORT + "/" + DB_NAME);
-        props.setProperty(DataSourceFactory.JDBC_SERVER_NAME, SERVER_NAME);
-        props.setProperty(DataSourceFactory.JDBC_DATABASE_NAME, DB_NAME);
-        props.setProperty(DataSourceFactory.JDBC_PORT_NUMBER, "" + PORT);
+        props.setProperty(DataSourceFactory.JDBC_SERVER_NAME, config.getServerName());
+        props.setProperty(DataSourceFactory.JDBC_DATABASE_NAME, config.getDatabaseName());
+        props.setProperty(DataSourceFactory.JDBC_PORT_NUMBER, config.getPortNumberSt());
         props.setProperty(DataSourceFactory.JDBC_DATASOURCE_NAME, "testds");
-        		
+        props.setProperty(DataSourceFactory.JDBC_USER, config.getUser());
+        props.setProperty(DataSourceFactory.JDBC_PASSWORD, config.getPassword());
         
-        props.setProperty(DataSourceFactory.JDBC_USER, USER);
-        props.setProperty(DataSourceFactory.JDBC_PASSWORD, PASSWORD);
-                		
         dsf.createDataSource(props).getConnection().close();
     }
 
